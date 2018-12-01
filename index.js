10f00a87d1c1499a5766939aeccd3e51a79550e4
sigidhanafi <sigidhanafi@gmail.com>
2018-12-01 23:41:11 +0700

    path with regex

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ app.use('/static', express.static('public'))
 // we can access from http://localhost:3000/static/perfect-code.jpg
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
 app.post('/', (req, res) => res.send('Hello post method!'))
+
+app.get('/random.text', function (req, res) {
+  res.send('random.text')
+})
+
+app.get('/ab?cd', (req, res) => res.send('/ab?cd'))
+// acd or abcd
+
+app.all('/secret', (req, res, next) => {
+  console.log('Accessing the secret section ...')
+  return next()
+})
 
 app.listen(port, () => { console.log(`App now listen on port ${port}`) })
