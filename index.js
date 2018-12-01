50e0e34884d1ef504bc573d8a5dc1acc933d04b8
sigidhanafi <sigidhanafi@gmail.com>
2018-12-01 23:31:29 +0700

    use static file with prefix path

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use('/static', express.static('public'))
+// use static file, add prefix path /static
+// we can access from http://localhost:3000/static/perfect-code.jpg
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
 app.post('/', (req, res) => res.send('Hello post method!'))
 
 app.listen(port, () => { console.log(`App now listen on port ${port}`) })
