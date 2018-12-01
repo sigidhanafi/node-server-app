96bd2b70b16f76c24215d1fae36b5faec6d0b8f5
sigidhanafi <sigidhanafi@gmail.com>
2018-12-01 23:29:01 +0700

    use express middleware to load static file

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/', (req, res) => res.send('Hello post method!'))
 
 app.listen(port, () => { console.log(`App now listen on port ${port}`) })
